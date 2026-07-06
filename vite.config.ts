acef98b3c1227cf3d5d9a0b474b704044c97267b
Chad <lumpsrgood@gmail.com>
2026-07-06 17:45:36 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/opportunitycentraldash/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
